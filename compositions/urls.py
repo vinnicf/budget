aef5c3cdd90cd81d89433db452a454e06a46fe9b
Vini Costa <vinnicf@gmail.com>
2023-09-26 11:12:26 -0300

    Add some basic detail and list views

--- compositions/urls.py
+++ compositions/urls.py
@@ -6,4 +6,5 @@ app_name = 'compositions'
 
 urlpatterns = [
     path('composition/<str:codigo>/', views.composition_detail, name='composition_detail'),
+    path('list/', views.composition_list, name='composition_list'),
 ]
--- compositions/views.py
+++ compositions/views.py
@@ -4,3 +4,8 @@ from .models import Composition
 def composition_detail(request, codigo):
     composition = get_object_or_404(Composition, codigo=codigo)
     return render(request, 'compositions/composition_detail.html', {'composition': composition})
+
+
+def composition_list(request):
+    compositions = Composition.objects.all()
+    return render(request, 'compositions/composition_list.html', {'compositions': compositions})
